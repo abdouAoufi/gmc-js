67c5753cefcf59d40b7c2b9a8405f8654a3f7861
AoufiAbderahmane <aoufi.abderahmane@lastingdynamics.com>
2022-07-02 10:05:12 +0100

    declare dom variables

---
+++ part-4/auth.js
@@ -0,0 +1,7 @@
+/* ----------- DECLARE DOME ELEMENTS ----------- */
+const DOM = document;
+const firstNameInp = DOM.getElementById("first-name-input");
+const lastNameInp = DOM.getElementById("last-name-input");
+const emailInp = DOM.getElementById("email-input");
+const passwordInput = DOM.getElementById("password-input");
+const signupBtn = DOM.getElementById("signup-button");
