af260a9e67cd4650022d34dbd5dcbef6d441b8c6
AoufiAbderahmane <aoufi.abderahmane@lastingdynamics.com>
2022-07-02 14:51:04 +0100

    add delete functionality

--- part-5/app.js
+++ part-5/app.js
@@ -8,7 +8,7 @@ const six = document.getElementById("six");
 const seven = document.getElementById("seven");
 const eight = document.getElementById("eight");
 const nine = document.getElementById("nine");
-const deleteBth = document.getElementById("del");
+const deleteBtn = document.getElementById("delete");
 const equalBth = document.getElementById("equal");
 const resetBth = document.getElementById("reset");
 const multiplyBtn = document.getElementById("multiply");
@@ -25,46 +25,62 @@ let operation = ""; // + - * /
 let tempFirstNumber = "";
 
 numbersBtn.forEach((element, i) => {
-  element.addEventListener("click", () => write(i));
+    element.addEventListener("click", () => write(i));
 });
 
 operationBtn.forEach((element) => {
-  element.addEventListener("click", () => {
-    operation = element.innerText;
-    tempFirstNumber = number;
-    result.innerText = "0";
-    number = "";
-  });
+    element.addEventListener("click", () => {
+        operation = element.innerText;
+        tempFirstNumber = number;
+        result.innerText = "0";
+        number = "";
+    });
 });
 
 function write(index) {
-  number = number + index;
-  result.innerText = number;
+    number = number + index;
+    result.innerText = number;
 }
 
 const doOperation = () => {
-  let currentResult = 0;
-  switch (operation) {
-    case "+":
-      currentResult = parseInt(tempFirstNumber) + parseInt(number);
-      break;
-    case "x":
-      currentResult = parseInt(tempFirstNumber) * parseInt(number);
-      break;
-    case "-":
-      currentResult = parseInt(tempFirstNumber) - parseInt(number);
-      break;
-    case "/":
-      currentResult = parseInt(tempFirstNumber) / parseInt(number);
-      break;
-  }
-  result.innerText = currentResult;
-  number = currentResult + "";
+    let currentResult = 0;
+    switch (operation) {
+        case "+":
+            currentResult = parseInt(tempFirstNumber) + parseInt(number);
+            break;
+        case "x":
+            currentResult = parseInt(tempFirstNumber) * parseInt(number);
+            break;
+        case "-":
+            currentResult = parseInt(tempFirstNumber) - parseInt(number);
+            break;
+        case "/":
+            currentResult = parseInt(tempFirstNumber) / parseInt(number);
+            break;
+    }
+    result.innerText = currentResult;
+    number = currentResult + "";
 };
 
+const deleteLastChar = () => {
+    const listChar = number.split("");
+    if (listChar.length === 1) {
+        number = "";
+        return result.innerText = "0";
+    }
+    const newList = listChar.map((c, i) => {
+        if (i < listChar.length - 1) {
+            return c;
+        }
+    });
+    number = newList.join("");
+    result.innerText = newList.join("");
+};
+
+deleteBtn.addEventListener("click", deleteLastChar);
 equalBth.addEventListener("click", doOperation);
 resetBth.addEventListener("click", () => {
-  tempFirstNumber = "";
-  number = "";
-  result.innerText = "0";
+    tempFirstNumber = "";
+    number = "";
+    result.innerText = "0";
 });
