f11335ee6b9c3d5ce8e55592e71fd84482b2c873
AoufiAbderahmane <aoufi.abderahmane@lastingdynamics.com>
2022-07-02 10:44:15 +0100

    add dom & validation

--- part-4/auth.js
+++ part-4/auth.js
@@ -5,3 +5,58 @@ const lastNameInp = DOM.getElementById("last-name-input");
 const emailInp = DOM.getElementById("email-input");
 const passwordInput = DOM.getElementById("password-input");
 const signupBtn = DOM.getElementById("signup-button");
+
+let firstName = "";
+let lastName = "";
+let email = "";
+let password = "";
+
+/* ----------- EVENT LISTENERS ----------- */
+
+const callback = () => {
+  firstName = firstNameInp.value;
+  lastName = lastNameInp.value;
+  email = emailInp.value;
+  password = passwordInput.value;
+
+  // VALIDATION
+  const validationResult = validInformation(
+    firstName,
+    lastName,
+    email,
+    password
+  );
+
+  if (validationResult === false) {
+    return;
+  }
+
+  const user = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    password, // ES6 😍
+  };
+};
+
+const validInformation = (firstName, lastName, email, password) => {
+  if (firstName.length <= 2) {
+    alert("first name should have at least 2 carecters");
+    return false;
+  }
+  if (lastName.length <= 2) {
+    alert("last name should have at least 2 carecters");
+    return false;
+  }
+  if (email.length <= 5) {
+    alert("email should have at least 2 carecters");
+    return false;
+  }
+  if (email.length <= 8) {
+    alert("password should have at least 8 carecters");
+    return false;
+  }
+  return true;
+};
+
+signupBtn.addEventListener("click", callback);
